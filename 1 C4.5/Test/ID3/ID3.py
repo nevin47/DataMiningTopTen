a04f4492434e9c59661aa3169c178f1fe82cf3e7
nevin47 <nevin47123@126.com>
2015-07-06 21:54:22 +0800

    借用ID3算法

---
+++ 1 C4.5/Test/ID3/ID3.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import trees
+import treePlotter
+fr = open('/Users/nevin47/Desktop/项目/TopTen_Python/DataMiningTopTen/1 C4.5/Test/ID3/lenses.txt')
+#导入数据
+lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+#设置标签
+lensesLabels = ['age','prescript','astigmatic','tearRate']
+#生成树
+lensesTree = trees.createTree(lenses,lensesLabels)
+#画图
+treePlotter.createPlot(lensesTree)
